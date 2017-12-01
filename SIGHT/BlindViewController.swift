05e3ab7148e5ce1c548ed8d64152035d9d7accda
Samuel Gray <sam.gray22@gmail.com>
2017-12-01 19:50:26 +0000

    Timer.

--- SIGHT/BlindViewController.swift
+++ SIGHT/BlindViewController.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
+import AVKit
+import Vision
+import AVFoundation
 
 class BlindViewController: UIViewController {
-    
+    var introTimer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //start timer
+        introTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(runTimeCode), userInfo: nil, repeats: true)
+
         
     }
     
+    //timer funcation
+    @objc func runTimeCode() {
+        print("hello")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
