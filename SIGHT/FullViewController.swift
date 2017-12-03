810ee9fa68f2955195ae74c81a13f6e1b141df6f
Samuel Gray <sam.gray22@gmail.com>
2017-12-03 20:10:32 +0000

    removed memory warning

--- SIGHT/FullViewController.swift
+++ SIGHT/FullViewController.swift
@@ -42,11 +42,6 @@ class FullSightViewController: UIViewController, UINavigationControllerDelegate 
         present(picker, animated: true)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
 }
 
